abf685416f16cfce5093b6a48c931876802bc6fe
17n71 <narek07012020@gmail.com>
2024-03-13 10:35:54 +0400

    change domain

--- routes.js
+++ routes.js
@@ -7,6 +7,7 @@ const getPixelResponse = res => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Content-type", "application/json");
   // res.end(Buffer.from(whitePixel, "base64"));
+
   res.end();
 };
 
@@ -19,7 +20,7 @@ router.post("/auth", function (req, res, next) {
     res.cookie(cookieName, "test", {
       maxAge: getMaxAge(),
       httpOnly: false,
-      domain: ".cookie-front.vercel.app",
+      domain: "https://cookie-front.vercel.app",
       secure: true,
       sameSite: "none",
     });
@@ -36,7 +37,7 @@ router.get("/test", function (req, res, next) {
   res.cookie("new-cookie", "value", {
     maxAge: getMaxAge(),
     httpOnly: false,
-    domain: ".cookie-front.vercel.app",
+    domain: "https://cookie-front.vercel.app",
     secure: false,
     sameSite: "none",
   });

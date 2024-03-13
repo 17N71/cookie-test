da4ab2cec63db8ddbe6e1fa26d5b3cd5450ea9de
17n71 <narek07012020@gmail.com>
2024-03-13 12:25:31 +0400

    netlify origin

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://cookie-front.vercel.app",
+    origin: "https://cookiefront.netlify.app",
     credentials: true,
     allowedHeaders: true,
   })

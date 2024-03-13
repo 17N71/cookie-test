ac74c74e1e8abe260a1acd88cf8f464cf019405f
17n71 <narek07012020@gmail.com>
2024-03-13 22:33:42 +0400

    remove dot before domain

--- routes.js
+++ routes.js
@@ -20,7 +20,7 @@ router.post("/auth", function (req, res, next) {
     res.cookie(cookieName, "test", {
       maxAge: getMaxAge(),
       httpOnly: false,
-      domain: ".netlify.app",
+      domain: "netlify.app",
       secure: true,
       sameSite: "none",
     });
@@ -37,7 +37,7 @@ router.get("/test", function (req, res, next) {
   res.cookie("new-cookie", "value", {
     maxAge: getMaxAge(),
     httpOnly: false,
-    domain: ".netlify.app",
+    domain: "netlify.app",
     secure: true,
     sameSite: "none",
   });

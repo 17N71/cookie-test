2da6cdab55ef244816d107a3831cfbeaf66fd01f
17n71 <narek07012020@gmail.com>
2024-03-13 12:30:48 +0400

    change domain

--- build/routes.js
+++ build/routes.js
@@ -18,7 +18,7 @@ router.post("/auth", function (req, res, next) {
     res.cookie(cookieName, "test", {
       maxAge: getMaxAge(),
       httpOnly: false,
-      domain: "netlify.app",
+      domain: "cookiefront.netlify.app",
       secure: true,
       sameSite: "none",
     });
@@ -35,7 +35,7 @@ router.get("/test", function (req, res, next) {
   res.cookie("new-cookie", "value", {
     maxAge: getMaxAge(),
     httpOnly: false,
-    domain: "netlify.app",
+    domain: "cookiefront.netlify.app",
     secure: true,
     sameSite: "none",
   });
